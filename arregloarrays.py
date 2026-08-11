4fefb38f49e5f374f67d4a656ba7ab872f0d3abc
JOEL <guanoluisajoel503@gmail.com>
2026-08-11 12:24:38 -0500

    Mensaje de lo que hiciste

---
+++ arregloarrays.py
@@ -0,0 +1,12 @@
+# 1. Crear un arreglo con los números: 10, 20, 30, 40 y 50
+numeros = [10, 20, 30, 40, 50]
+
+# 2. Mostrar en pantalla el valor ubicado en la posición 1 del arreglo (el número 20)
+print("El valor en la posición 1 es:", numeros[1])
+print("-" * 35)
+
+# 3. Recorrer todo el arreglo desde la primera posición y mostrarlos en consola
+print("Todos los números del arreglo:")
+for num in numeros:
+    print(num)
+    
