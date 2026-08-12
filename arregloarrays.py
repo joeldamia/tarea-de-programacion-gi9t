c40b1ec41fe0b5c7443c4a6b0039555ee7b8ccc3
janpierguartazaca <guanoluisajoel503@gmail.com>
2026-08-11 19:06:24 -0500

    Mi primer commic

--- arregloarrays.py
+++ arregloarrays.py
@@ -3,7 +3,7 @@ numeros = [10, 20, 30, 40, 50]
 
 # 2. Mostrar en pantalla el valor ubicado en la posición 1 del arreglo (el número 20)
 print("El valor en la posición 1 es:", numeros[1])
-print("-" * 35)
+print("-" * 25)
 
 # 3. Recorrer todo el arreglo desde la primera posición y mostrarlos en consola
 print("Todos los números del arreglo:")
