71c47a8c6859c4a2a5a908f238e335f601d9d640
Joel Guanoluisa <guanoluisajoel503@gmail.com>
2026-08-21 10:11:10 -0500

    Format matrix output and add comments

--- Debere.py
+++ Debere.py
@@ -1,9 +1,16 @@
+## MATRIZ 3X3
+##Deber Semana 10
 matriz = [
-        [2, 4, 6],
-        [1, 3, 5],
-        [7, 8, 9]
+    [2, 4, 6],
+    [1, 3, 5],
+    [7, 8, 9]
 ]
 
+print("VALORES DE MATRIZ")
+
+print("__________")
 for i in range(3):
     for j in range(3):
-        print(matriz[i][j])
+        print(matriz[i][j], end="  ")
+    print()
+    print("__________")
